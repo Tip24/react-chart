68f6127e897cb74fd944a130c3e38ae750dfb195
Tip24 <56535193+Tip24@users.noreply.github.com>
2020-02-15 16:57:36 +0400

    Add files via upload

---
+++ precache-manifest.9f6f557737473092cd4e8fe63a0724ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3596c735c520c9086b4a6527dd27772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "448a15f9018709437d63",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "80ab6c05c6babe7dfb89",
+    "url": "/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "448a15f9018709437d63",
+    "url": "/static/js/2.b9562e49.chunk.js"
+  },
+  {
+    "revision": "2a7016412afc226bfd5ce83c577457f9",
+    "url": "/static/js/2.b9562e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ab6c05c6babe7dfb89",
+    "url": "/static/js/main.c5543db9.chunk.js"
+  },
+  {
+    "revision": "08d61dde89e301c336e3",
+    "url": "/static/js/runtime-main.bcafc99c.js"
+  }
+]);
